c60ed316f5a61ba26c62d805f5cd901325ad35a5
Genesis Agravio <agraviog@gmail.com>
2021-08-10 00:10:10 +0800

    no commit

--- splide.js
+++ splide.js
@@ -1,20 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    new Splide(".splide", {
-      perPage: 3,
-      focus: "0",
-      trimSpace: false,
-      gap: "2em",
-      perMove: 1,
-      rewind: true,
-      breakpoints: {
-        768: {
-          perPage: 2,
-          gap: "1.5em",
-        },
-        480: {
-          perPage: 1,
-          gap: "1.5em",
-        },
+  new Splide(".splide", {
+    perPage: 3,
+    focus: "0",
+    trimSpace: false,
+    gap: "2em",
+    perMove: 1,
+    rewind: true,
+    breakpoints: {
+      768: {
+        perPage: 2,
+        gap: "1.5em",
+        rewind: true,
       },
-    }).mount();
+      480: {
+        perPage: 1,
+        gap: "1.5em",
+        rewind: true,
+      },
+    },
+  }).mount();
 });
